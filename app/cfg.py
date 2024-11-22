c2ee4ba4f1a8740e3d44280762541bde8c7fdcf2
v-49 <140229616+v-49@users.noreply.github.com>
2024-11-22 17:10:30 +0800

    Update cfg.py

--- app/cfg.py
+++ app/cfg.py
@@ -2,8 +2,7 @@
 import os
 #exp:"mysql+pymysql://user:password@host:port/database"
 DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-"mysql+pymysql://root:mysql_THhGnA@119.254.155.171:23306/airport"
+    "DATABASE_URL"
 )
 if not DATABASE_URL:
     raise ValueError("环境变量 DATABASE_URL 未设置，请配置数据库连接字符串。")
